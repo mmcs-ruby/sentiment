d495844275ca23d3482bbd6b184542d334a6fdee
DaaRiya <58295894+DaaRiya@users.noreply.github.com>
2021-11-19 09:50:22 +0300

    see Base

--- lib/sentiment.rb
+++ lib/sentiment.rb
@@ -3,6 +3,7 @@
 require_relative "sentiment/version"
 require_relative 'sentiment/tfidf'
 require_relative "sentiment/text_preprocessor"
+require_relative 'sentiment/Base_model'
 
 module Sentiment
   class Error < StandardError; end
